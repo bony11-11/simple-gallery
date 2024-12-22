d48899cbf52f32a74805991c5eb9612516984e54
munzman <munzx@munzlab.munzlab.com>
2024-12-22 10:05:52 +0400

    fix color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -30,7 +30,7 @@
 		</h3>
 	</div>
 	<div class="w-full h-full">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f17eb8" fill-opacity="1" d="M0,288L60,272C120,256,240,224,360,213.3C480,203,600,213,720,213.3C840,213,960,203,1080,170.7C1200,139,1320,85,1380,58.7L1440,32L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
+		<svg xmlns="http://www.w3.org/2000/svg" class=" opacity-100" viewBox="0 0 1440 320"><path fill="#f472b6" fill-opacity="1" d="M0,288L60,272C120,256,240,224,360,213.3C480,203,600,213,720,213.3C840,213,960,203,1080,170.7C1200,139,1320,85,1380,58.7L1440,32L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
 	</div>
 </header>
 
@@ -74,11 +74,11 @@
 	</div>
 </section>
 
-<footer class="w-full text-center">
-	<div class="w-full h-full">
-		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f17eb8" fill-opacity="1" d="M0,288L60,272C120,256,240,224,360,213.3C480,203,600,213,720,213.3C840,213,960,203,1080,170.7C1200,139,1320,85,1380,58.7L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+<footer class="w-full text-center p-0 m-0">
+	<div class="w-full h-full inset-0">
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#f472b6" fill-opacity="1" d="M0,288L60,272C120,256,240,224,360,213.3C480,203,600,213,720,213.3C840,213,960,203,1080,170.7C1200,139,1320,85,1380,58.7L1440,32L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
 	</div>
-	<h1 class="p-4 m-0 text-lg font-bold font-sans uppercase bg-pink-400 text-white underline underline-offset-8 italic">
+	<h1 class="p-8 m-0 text-lg font-bold font-sans uppercase bg-pink-400 text-white underline underline-offset-8 italic">
 		Nosa Gallery
 	</h1>
 </footer>
