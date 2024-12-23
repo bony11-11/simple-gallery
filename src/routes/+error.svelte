a62e59a23002e18e1061b9b09cdab7f856e667ea
Moheera <moheeradotcom@gmail.com>
2024-12-23 20:21:24 +0400

    Create +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<section class="w-full h-full p-12 flex flex-col gap-4 items-center justify-center">
+    <h1 class=" text-7xl font-semibold">404</h1>
+    <hr />
+    <p class=" text-lg">
+        Go to <a class=" underline font-semibold text-blue-400" href="/">home page.</a>
+    </p>
+</section>
